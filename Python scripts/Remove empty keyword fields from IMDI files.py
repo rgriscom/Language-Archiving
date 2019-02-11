e99042ec36b6962043f5d4985a44f39c0632fcf2
rgriscom <46093597+rgriscom@users.noreply.github.com>
2019-02-11 14:02:18 -0800

    Update Remove empty keyword fields from IMDI files.py

--- Python scripts/Remove empty keyword fields from IMDI files.py
+++ Python scripts/Remove empty keyword fields from IMDI files.py
@@ -1,13 +1,31 @@
-# Script author: Richard Griscom (rgriscom@uoregon.edu), December 2018
-# Script description: This script is designed to remove any empty Keyword fields in IMDI files. It makes new copies of all of the IMDI files in the first specified folder ("FOLDER FOR ORIGINAL FILES"), puts them in a new folder ("FOLDER FOR NEW FILES"). In order to make this script work correctly, you need to replace "FOLDER FOR ORIGINAL FILES" (two instances) and "FOLDER FOR NEW FILES" (one instance) in the script below with the corresponding directories on your machine. After completing your metadata, you can use another script to remove any remaining empty "Keyword" fields. 
+# Script author: Richard Griscom (rgriscom@uoregon.edu), February 2019
+# Script description: This script is designed to remove any empty Keyword fields in IMDI files. It makes new copies of all of the IMDI files in same folder as the script, and puts them in a new folder ("Output"). 
 
-import os
+import os, platform, shutil
+if platform.system() == 'Windows':
+    system_var = 'w'
+    print('OS is Windows')
+else:
+    system_var = 'nw'
+        
 
-dir_list = os.listdir('/FOLDER FOR ORIGINAL FILES/')
+#!!!Input and output directories!!!
+if system_var == 'w':
+    input_dir = os.getcwd() + "\\"
+    output_dir = os.getcwd() + "\\Output\\"
+    print('Input dir: ' + input_dir)
+    print('Output dir: ' + output_dir) 
+else:
+	input_dir = os.getcwd() + "/"
+	output_dir = os.getcwd() + "/Output/"
+        print('Input dir: ' + input_dir)
+        print('Output dir: ' + output_dir)
+
+dir_list = os.listdir(input_dir)
 for d in dir_list:
     if ".imdi" in d:
-        oldname = "/FOLDER FOR ORIGINAL FILES/" + d
-        newname = "/FOLDER FOR NEW FILES/" + d
+        oldname = input_dir + d
+        newname = output_dir + d
         with open(oldname) as f_old, open(newname, "w") as f_new:
            
             for line in f_old:
@@ -16,5 +34,6 @@ for d in dir_list:
                 else:
                     f_new.write(line)
             print(d)
+
  
                     
