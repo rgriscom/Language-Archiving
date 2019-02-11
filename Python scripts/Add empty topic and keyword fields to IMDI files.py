030543c8f81c680712138635daf99e81ced5a092
rgriscom <46093597+rgriscom@users.noreply.github.com>
2019-02-11 13:58:26 -0800

    Update Add empty topic and keyword fields to IMDI files.py

--- Python scripts/Add empty topic and keyword fields to IMDI files.py
+++ Python scripts/Add empty topic and keyword fields to IMDI files.py
@@ -1,23 +1,53 @@
-# Script author: Richard Griscom (rgriscom@uoregon.edu), December 2018
-# Script description: This script is designed to make it easier to enter multiple keys into IMDI files for the purpose of preparing metadata for deposit in the Endangered Languages Archive. It makes new copies of all of the IMDI files in the first specified folder ("FOLDER FOR ORIGINAL FILES"), puts them in a new folder ("FOLDER FOR NEW FILES"), and adds six empty Key fields to each file: one called "Topic", and five called "Keyword". In order to make this script work correctly, you need to replace "FOLDER FOR ORIGINAL FILES" (two instances) and "FOLDER FOR NEW FILES" (one instance) in the script below with the corresponding directories on your machine. After completing your metadata, you can use another script to remove any remaining empty "Keyword" fields. 
+# Script author: Richard Griscom (rgriscom@uoregon.edu), February 2019
+# Script description: This script is designed to make it easier to enter multiple keys into IMDI files for the purpose of preparing metadata for deposit in the Endangered Languages Archive. It makes new copies of all of the IMDI files in the same folder as the script, puts them in a new folder called 'Output,' and adds six empty Key fields to each file: one called "Topic", and five called "Keyword". After completing your metadata, you can use another script to remove any remaining empty "Keyword" fields. 
 
 
-import os
-dir_list = os.listdir('/FOLDER FOR ORIGINAL FILES/')
+import os, platform, shutil
+if platform.system() == 'Windows':
+    system_var = 'w'
+    print('OS is Windows')
+else:
+    system_var = 'nw'
+        
+
+#!!!Input and output directories!!!
+if system_var == 'w':
+    input_dir = os.getcwd() + "\\"
+    output_dir = os.getcwd() + "\\Output\\"
+    print('Input dir: ' + input_dir)
+    print('Output dir: ' + output_dir) 
+else:
+	input_dir = os.getcwd() + "/"
+	output_dir = os.getcwd() + "/Output/"
+print('Input dir: ' + input_dir)
+print('Output dir: ' + output_dir)
+
+
+
+dir_list = os.listdir(input_dir)
+if "Output" in dir_list:
+    try:
+        shutil.rmtree(output_dir)
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+os.mkdir(output_dir)
+
 for d in dir_list:
     if ".imdi" in d:
-        oldname = "/FOLDER FOR ORIGINAL FILES/" + d
-        newname = "/FOLDER FOR NEW FILES/" + d
+        oldname = input_dir + d
+        newname = output_dir + d
         with open(oldname) as f_old, open(newname, "w") as f_new:
             check = 0
             for line in f_old:
-                if check == 1 and "Keys" in line:
-                    check =0
+                if "<Content>" in line:
+                    check = 1
+                if "</Content>" in line:
+                    check = 2
+                if check == 1 and "<Keys/>" in line:
+                    f_new.write('\t<Keys>\n\t\t<Key Name=\"Topic\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t</Keys>\n')
                 else:
                     f_new.write(line)
-                    if "</Project>" in line:
-                        f_new.write('\t<Keys>\n\t\t<Key Name=\"Topic\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t\t<Key Name=\"Keyword\"></Key>\n\t</Keys>\n')
-                        check = 1
-            print(d)
+                    
+            print('Completed processing of: ' + d)
  
                     
